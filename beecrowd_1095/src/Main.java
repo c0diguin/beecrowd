c536141e51d3dbe703cae83c96ffbf84447d0059
c0diguin <codiguin@git.com>
2025-08-03 12:24:05 -0300

    beecrowd 1095: Sequencia IJ 1

---
+++ beecrowd_1095/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int j = 60;
+        int i = 1;
+
+        while (j >= 0) {
+            System.out.println("I=" + i + " J=" + j );
+
+            i += 3;
+            j -= 5;
+        }
+
+    }
+}
