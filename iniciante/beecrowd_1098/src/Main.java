c5cbb025454f6817b9cd6ed9a8171517f9676c3f
c0diguin <codiguin@git.com>
2025-08-08 16:51:09 -0300

    reestruturação dos exercícios iniciais

---
+++ iniciante/beecrowd_1098/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        for (double i = 0; i <= 2.01; i += 0.2) {
+            for (int j = 1; j < 4; j++) {
+                if (i == 0 || i == 1 || i >= 2) {
+                    System.out.printf("I=%.0f J=%.0f\n", i, j + i);
+                } else {
+                    System.out.printf("I=%.1f J=%.1f\n", i, j + i);
+                }
+            }
+        }
+    }
+}
